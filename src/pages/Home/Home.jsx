446958aa7945914ac26d2fc9821f904de41b7fe8
sojwal574 <sojwalvilhekar94@gmail.com>
2025-06-27 23:29:01 +0530

    home updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,35 @@ import React, { useContext, useEffect, useState } from 'react'
 import './Home.css'
 import { CoinContext } from '../../context/CoinContext';
 
+// to display the charts we need to import Link
+import {Link} from 'react-router-dom'
+// we will use the Link so that when people will click on the element it will open another page
+// replace tableLayout div with Link
+
+
+
 
 const Home = () => {
+
+    // ! Search functionality in the webpage
+    const [input , setInput] = useState('');
+    const inputHandler = (event) => {
+      setInput(event.target.value);
+
+      // now if we clear the input field table should display as it is
+      if(event.target.value === "") {
+        setDisplayCoin(allCoin)
+      }
+    }
+
+    const searchHandler = async (event) => {
+      event.preventDefault();
+      const coins =  await allCoin.filter((item)=> {
+        return item.name.toLowerCase().includes(input.toLowerCase())
+      })
+      // bitcoin , now if we type bit then it will display the filtered data
+      setDisplayCoin(coins) // display the filtered data in the table
+    }
 
 
   const {allCoin , currency} = useContext(CoinContext);
@@ -12,6 +39,7 @@ const Home = () => {
   useEffect(() => {
     setDisplayCoin(allCoin);
   } , [allCoin])
+
 
   return (
     <div className='Home'>
@@ -26,9 +54,21 @@ const Home = () => {
         </p>
 
         {/* for searching the crypto */}
-        <form>
+        <form onSubmit={searchHandler}>
+          {/* using list attribute the coin data will be displayed in the input field */}
+          <input onChange = {inputHandler} list='coinlist' value={input} type="text" placeholder='Search Crypto' required/>
+          
+          {/* We need to add the suggestions in the search field */}
 
-          <input type="text" placeholder='Search Crypto'/>
+            <datalist id='coinlist'>
+
+              { allCoin.map((item , index) => (<option key={index} value={item.name}/>
+              )) }
+
+
+            </datalist>
+          
+          
           <button type='submit'>Search</button>
 
         </form>
@@ -44,10 +84,13 @@ const Home = () => {
             <p style={{textAlign:'center'}}>24H Change</p>
             <p className='market-cap'>Market Cap</p>
           </div>
+
+          {/*  Display the data in the table */}
           {
           displayCoin.slice(0,10).map((item, index) => (
 
-              <div className='table-layout' key={index}>
+            // The 'to' property will redirect us from the home page to the coin page
+              <Link to={`/coin/${item.id}`} className='table-layout' key={index}>
 
                   <p>{item.market_cap_rank}</p>
                   <div>
@@ -63,15 +106,18 @@ const Home = () => {
 
                   <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
 
-              </div>
+              </Link>
 
 
           ))
         }
+
         </div>
 
     </div>
   )
 }
+
+
 
 export default Home
