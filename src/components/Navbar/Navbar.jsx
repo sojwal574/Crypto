70d209c394fa0ac7df33c0c0583ee87c9767db27
sojwal574 <sojwalvilhekar94@gmail.com>
2025-06-30 13:49:50 +0530

    navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext , useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
@@ -8,10 +8,36 @@ import { Link } from 'react-router-dom' // here we use link so that when \
 // we are on some page and by clicking on the logo or the 
 // Home option we will get redirected to the home page 
 
+// for signout
+import { signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../Firebase/config';
+import { useNavigate } from 'react-router-dom';
+
 
 
 
 const Navbar = () => {
+
+
+    // for signout purpose
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+      });
+      return () => unsubscribe();
+    }, []);
+
+    const handleLogout = async () => {
+      await signOut(auth);
+      navigate('/');
+    };
+
+
+
+
 
   const {setCurrency} = useContext(CoinContext)
 
@@ -65,8 +91,25 @@ const Navbar = () => {
                 <option value="inr">INR</option>
             </select>
 
-            <button>Sign up <img src= {arrow_icon} alt="" /> </button>
+            {/* <button>Sign up <img src= {arrow_icon} alt="" /> </button>*/}
+            {/* Make the button link so that we can use it for signup */}
+            {/* <Link to="/signup">
+              <button>
+                Sign up <img src={arrow_icon} alt="" />
+              </button>
+            </Link> */}
 
+
+
+              {/*when logged in signup and login changes to logout  */}
+            {!user ? (
+              <>
+                <Link to='/signup'><button>Sign up <img src={arrow_icon} alt='' /></button></Link>
+                <Link to='/login'><button>Login <img src={arrow_icon} alt='' /></button></Link>
+              </>
+            ) : (
+              <button onClick={handleLogout}>Logout <img src={arrow_icon} alt='' /></button>
+            )}
 
         </div>
 
